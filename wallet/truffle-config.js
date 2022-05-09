ef8013b8d2ba53a5ebc82fbc8db9fa38028a3c08
Jules <64536366+BoomJules@users.noreply.github.com>
2022-05-09 12:42:24 +0200

    Multisig Wallet Dapp

---
+++ wallet/truffle-config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const fs = require('fs');
+const provider = require('@truffle/hdwallet-provider');
+const secrets = JSON.parse(
+  fs.readFileSync('.secrets.json').toString().trim()
+);
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a development blockchain for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+
+  networks: {
+    kovan: {
+      provider: () =>
+        new provider(
+          secrets.privateKeys,
+          'https://kovan.infura.io/v3/b03745ce536345b9b365625fa77a7b5d',
+          0,
+          3
+        ),
+      network_id: 42
+    }
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled:
+  // false to enabled: true. The default storage location can also be
+  // overridden by specifying the adapter settings, as shown in the commented code below.
+  //
+  // NOTE: It is not possible to migrate your contracts to truffle DB and you should
+  // make a backup of your artifacts to a safe location before enabling this feature.
+  //
+  // After you backed up your artifacts you can utilize db by running migrate as follows:
+  // $ truffle migrate --reset --compile-all
+  //
+  // db: {
+    // enabled: false,
+    // host: "127.0.0.1",
+    // adapter: {
+    //   name: "sqlite",
+    //   settings: {
+    //     directory: ".db"
+    //   }
+    // }
+  // }
+};
